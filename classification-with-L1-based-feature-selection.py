c1f6c8b462dc91b6210a0e2c36a10efe8e94a60f
Renan Cunha <renan.bara@hotmail.com>
2017-12-14 21:32:23 -0200

    changed the markdown of notebook and fixed some issues

--- classification-with-L1-based-feature-selection.py
+++ classification-with-L1-based-feature-selection.py
@@ -1,10 +1,11 @@
 
 # coding: utf-8
 
-# In[321]:
+# Import libraries:
+
+# In[67]:
 
 
-# used libraries
 import pandas as pd
 import numpy as np
 from sklearn.svm import LinearSVC
@@ -23,48 +24,76 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
 
-# In[322]:
+# ### Dataset
+# This dataset contains audio features (36 features) extracted from 280 samples. For each feature, we have 6 statistics (mean, median, std, std by mean, max and min.
+# 
+# So, we expect a 280x(216+1) matrix, where the last column is the "label" (1 = good roof-tile or 2 = roof-tile with problems).
+
+# In[68]:
 
 
-# import the dataset
 dt = pd.read_csv("dataset.csv")
 dt.shape
 
 
-# In[323]:
+# Lets see how our data looks like:
+
+# In[69]:
 
 
-# separate data (X and Y)
+dt.head()
+
+
+# Lets separate the data:
+# 
+# X will be a matrix containing all features from all samples.
+# 
+# Y will be a vector containing the labels of all observations.
+
+# In[70]:
+
+
 array = dt.values
 X = array[:, 0:216]
 y = array[:, 216]
-validation_size = 0.30
-seed = 7
 X.shape
 
 
-# In[324]:
+# Now, we need to make the data sets to train and to validate the models. The choosed proportion is: 70% to test and 30% to validate.
+
+# In[71]:
 
 
-X_train, X_validation, y_train, y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+validation_size = 0.30
+seed = 7
+X_train, X_validation, y_train, y_validation = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
 X_train.shape
 
 
-# In[325]:
+# ### L1-based feature selection
+# Our dataset contains a lot of features (216 to be more specific).
+# 
+# Some features are collinear, so we can and we must to transform our data. To do that, I choosed to use a L1-based feature selection method.
+# 
+# Its importante to say that smaller C implies in fewer features selected.
+
+# In[72]:
 
 
-# L1-based feature selection
 lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(X_train, y_train)
 modellsvc = SelectFromModel(lsvc, prefit=True)
 X_train_new = modellsvc.transform(X_train)
 X_train_new.shape
 
 
-# In[326]:
+# ### Select a classifier
+# We will evaluate six classifiers, to choose the best model to classify our validation data. The criteria to choose the best is the accuracy of the model on the train data.
+# 
+# We use a cross-validation (k-fold with k = 10) to evaluate the models
+
+# In[73]:
 
 
-# test six classifiers
-scoring = 'accuracy'
 models = []
 models.append(('LR', LogisticRegression()))
 models.append(('LDA', LinearDiscriminantAnalysis()))
@@ -73,6 +102,7 @@ models.append(('CART', DecisionTreeClassifier()))
 models.append(('NB', GaussianNB()))
 models.append(('SVM', SVC()))
 
+scoring = 'accuracy'
 results = []
 names = []
 for name, model in models:
@@ -84,14 +114,9 @@ for name, model in models:
 	print(msg)
 
 
-# In[327]:
+# Selected the best classifier model
 
-
-X_validation_new = modellsvc.transform(X_validation)
-X_validation_new.shape
-
-
-# In[328]:
+# In[74]:
 
 
 best_model_idx = results.index(max(results))
@@ -99,7 +124,20 @@ best_model = models[best_model_idx]
 best_model[0]
 
 
-# In[329]:
+# Transform the validation input data to reduce the number of features
+# We will use our modellsvc to do the feature selection here
+
+# In[75]:
+
+
+X_validation_new = modellsvc.transform(X_validation)
+X_validation_new.shape
+
+
+# ### Make the predictions on validation data
+# Finally, we evaluate the accuracy of our proposed model making the predictions of X_validation_new
+
+# In[76]:
 
 
 best_model[1].fit(X_train_new, y_train)
